a375fd7130384ddec3e5bd25b2f2186470a7578e
chicacode <gerahernan.al@gmail.com>
2020-09-04 13:52:17 +0200

    added icons

--- js/app.js
+++ js/app.js
@@ -5,16 +5,35 @@ console.log("Filter erp");
 // JS CSS manipulation
 // Eventlisteners
 // Inmediately inoked Function Expressions
-/* 2 different sections: 1. the filter buttons,
+/* Two different sections: 1. the filter buttons,
 whose purpose are filter the store items by class
 the 2 section is the searc filter, whose purpose is o filter the store items by search
 */
-let input;
-let buttonAll = document.querySelector('#all');
-let buttonCRM = document.querySelector('#crm');
-let buttonManufact = document.querySelector('#manufacturing');
-let buttonSP = document.querySelector('#supplychain');
-let buttonFin = document.querySelector('#financial');
-let buttonPM = document.querySelector('#projectmanag');
+const carrouselItem = document.querySelectorAll(".carrousel-item");
+const buttons = document.querySelectorAll(".main-buttons__services");
+
+//iterate over the button array for each button when i click it filters
+buttons.forEach(button => { //get the event target
+    button.addEventListener("click", (e) => {
+        e.preventDefault()
+        // get the value wit dataset filter
+        const filterValue = e.target.dataset.filter;
+
+        carrouselItem.forEach((element) => {
+            if (filterValue === 'all') {
+                element.style.display = 'inline-block';
+            } else {
+                if (element.classList.contains(filterValue)) {
+                    element.style.display = 'inline-block';
+                } else {
+                    element.style.display = 'none';
+                }
+            }
+        })
+    });
+});
+
+
+
 
 
