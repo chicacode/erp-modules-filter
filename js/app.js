5162a3b036a987042810d33e3a7749a47586c380
chicacode <gerahernan.al@gmail.com>
2020-09-04 11:00:50 +0200

    added variables and data-attribute

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log("Filter erp");
+// DOM manipulation
+// Control Structures
+// Array.Foreach
+// JS CSS manipulation
+// Eventlisteners
+// Inmediately inoked Function Expressions
+/* 2 different sections: 1. the filter buttons,
+whose purpose are filter the store items by class
+the 2 section is the searc filter, whose purpose is o filter the store items by search
+*/
+let input;
+let buttonAll = document.querySelector('#all');
+let buttonCRM = document.querySelector('#crm');
+let buttonManufact = document.querySelector('#manufacturing');
+let buttonSP = document.querySelector('#supplychain');
+let buttonFin = document.querySelector('#financial');
+let buttonPM = document.querySelector('#projectmanag');
+
+
