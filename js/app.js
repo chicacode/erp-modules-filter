a67d70d90e4e538983ebef1ac8083356a40374fb
chicacode <gerahernan.al@gmail.com>
2020-09-10 11:15:46 +0200

    added a css modal and slideshow

--- js/app.js
+++ js/app.js
@@ -12,6 +12,19 @@ the 2 section is the searc filter, whose purpose is o filter the store items by 
 const carrouselItem = document.querySelectorAll(".carrousel-item");
 const buttons = document.querySelectorAll(".main-buttons__services");
 let input = document.getElementById("search-input");
+let modal = document.querySelector(".lightbox-container");
+let modalItem = document.querySelector(".lightbox-holder ");
+let buttonModalClose = document.querySelector(".lightbox-close");
+let images = document.querySelectorAll(".store-img");
+let imageList = [];
+let imageCounter = 0;
+
+images.forEach(function (image) {
+    imageList.push(image.src);
+})
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
 
 //iterate over the button array for each button when i click it filters
 (function () {
@@ -51,6 +64,48 @@ let input = document.getElementById("search-input");
     })
 
 })();
+
+
+(function () {
+    carrouselItem.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            let image = e.target.src;
+
+            modalItem.style.backgroundImage = `url(${image})`;
+            modalItem.style.width = '90%';
+            modalItem.style.backgroundPosition = 'center';
+            modalItem.style.backgroundRepeat = 'no - repeat';
+            modalItem.style.backgroundSize = 'cover';
+            modal.style.display = 'block';
+
+        })
+
+        let btnLeft = document.querySelector('.btnLeft');
+        btnLeft.addEventListener('click', function () {
+            imageCounter--;
+            if (imageCounter < 0) {
+                imageCounter = imageList.length - 1;
+            }
+            modalItem.style.backgroundImage = `url(${imageList[imageCounter]})`
+        });
+        //select left button from the DOM
+        let btnRight = document.querySelector('.btnRight');
+        btnRight.addEventListener('click', function () {
+            imageCounter++;
+            if (imageCounter >= imageList.length) {
+                imageCounter = 0;
+            }
+            modalItem.style.backgroundImage = `url(${imageList[imageCounter]})`;
+        });
+
+        buttonModalClose.addEventListener('click', function () {
+            modal.style.display = 'none';
+        });
+    })
+
+})();
+
+
 
 
 
