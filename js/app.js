b60ca0c56fba8bff4bdfa8f025abd3da92f62e84
chicacode <gerahernan.al@gmail.com>
2020-09-20 16:04:10 +0200

    added an item product list and crud functionality

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 console.log("Filter erp");
-// DOM manipulation
-// Control Structures
-// Array.Foreach
-// JS CSS manipulation
-// Eventlisteners
-// Inmediately inoked Function Expressions
-/* Two different sections: 1. the filter buttons,
-whose purpose are filter the store items by class
-the 2 section is the searc filter, whose purpose is o filter the store items by search
-*/
+
 const carrouselItem = document.querySelectorAll(".carrousel-item");
 const buttons = document.querySelectorAll(".main-buttons__services");
+const cartContent = document.querySelector('#cart-list tbody');
+const cart = document.querySelector('#cart');
+const emptyCartBtn = document.querySelector('#empty-cart');
+const productList = document.querySelector('#list-products');
+
 let input = document.getElementById("search-input");
 let modal = document.querySelector(".lightbox-container");
 let modalItem = document.querySelector(".lightbox-holder ");
 let buttonModalClose = document.querySelector(".lightbox-close");
 let images = document.querySelectorAll(".store-img");
 let imageList = [];
+let cartItems = [];
 let imageCounter = 0;
 
 images.forEach(function (image) {
@@ -26,6 +23,7 @@ images.forEach(function (image) {
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
+loadListeners();
 //iterate over the button array for each button when i click it filters
 (function () {
     buttons.forEach(button => { //get the event target
@@ -104,7 +102,108 @@ let span = document.getElementsByClassName("close")[0];
 
 })();
 
+function loadListeners() {
+    productList.addEventListener('click', addProduct);
+}
 
+function addProduct(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('add-cart')) {
+        const itemSelected = e.target.parentElement.parentElement;
+        readProductData(itemSelected);
+    }
+}
+
+function deleteItem(e) {
+    if (e.target.classList.contains('borrar-curso')) {
+        const cursoId = e.target.getAttribute('data-id');
+
+        cartItems = cartItems.filter(item => item.id !== cursoId);
+
+        carritoHTML();
+    }
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la información del curso
+function readProductData(curso) {
+    // console.log(curso);
+
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    // Revisa si un elemento ya existe en el carrito
+    const existe = cartItems.some(curso => curso.id === infoCurso.id);
+    if (existe) {
+        // Actualizamos la cantidad
+        const cursos = cartItems.map(curso => {
+            if (curso.id === infoCurso.id) {
+                curso.cantidad++;
+                return curso; // retorna el objeto actualizado
+            } else {
+                return curso; // retorna los objetos que no son los duplicados
+            }
+        });
+        cartItems = [...cursos];
+    } else {
+        // Agrega elementos al arreglo de carrito
+        cartItems = [...cartItems, infoCurso];
+    }
+
+
+
+
+    console.log(cartItems);
+
+    carritoHTML();
+}
+
+
+// Muestra el Carrito de compras en el HTML
+function carritoHTML() {
+
+    // Limpiar el HTML
+    limpiarHTML();
+
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach(curso => {
+        const { imagen, titulo, precio, cantidad, id } = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width="100">
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}" > X </a>
+            </td>
+        `;
+
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+
+
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+    // Forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
 
 
 
